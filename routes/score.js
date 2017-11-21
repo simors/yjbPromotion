a3eddbfef0105046a86c8d0e05b722c71f310144
wanpeng <wanpeng19860919@163.com>
2017-11-21 16:49:40 +0800

    <Debug>增加错误提示

--- routes/score.js
+++ routes/score.js
@@ -6,13 +6,25 @@ var AV = require('leancloud-storage')
 router.get('/:id', function(req, res, next) {
   let userInfo = undefined
   var userId = req.params.id
-  AV.Cloud.run('authFuncTest', {userId: userId}).then((result) => {
+  AV.Cloud.run('authRequestUserInfo', {userId: userId}).then((result) => {
+    if(!result) {
+      res.render('error', {
+        title: "用户不存在",
+        description: "用户信息不存在，请联系客服"
+      })
+      return
+    }
     userInfo = result
     return AV.Cloud.run('promGetValidScoreExProm', {userId: userId})
   }).then((promotion) => {
-    console.log("promotion", promotion)
+    if(!promotion) {
+      res.render('error', {
+        title: "活动已失效",
+        description: "积分兑换活动已经失效"
+      })
+      return
+    }
     let gifts = promotion.awards.gifts
-    console.log("gifts", gifts)
     res.render('score', {
       userId: userId,
       promotionId: promotion.id,
@@ -20,6 +32,11 @@ router.get('/:id', function(req, res, next) {
       avatar: userInfo.avatar,
       score: userInfo.score,
       gifts: gifts,
+    })
+  }).catch((error) => {
+    res.render('error', {
+      title: "获取信息获取失败",
+      description: "错误代码：" + error.code
     })
   })
 });
