bc60cd776e4cacb1eda578e676def3bef207f1fa
wanpeng <wanpeng19860919@163.com>
2017-11-21 12:06:47 +0800

    <feat>兑换

--- routes/exchange.js
+++ routes/exchange.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
+var GLOBAL_CONFIG = require('../config')
 
 router.get('/:userId/:promotionId/:giftId', function(req, res, next) {
   var userId = req.params.userId
   var promotionId = req.params.promotionId
   var giftId = req.params.giftId
   res.render('exchange', {
+    appId: GLOBAL_CONFIG.LC_APP_ID,
+    appKey: GLOBAL_CONFIG.LC_APP_KEY,
     userId: userId,
     promotionId: promotionId,
     giftId: giftId,
